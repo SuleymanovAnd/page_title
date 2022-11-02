58832eaf19c3638d8862bcdbf6f078130b4847e1
Suleymanov Andrey <sasus37@gmail.com>
2022-11-02 20:14:47 +0300

    done

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <cpr/cpr.h>
+#include <algorithm>
 
 int main() {
 
-    cpr::Get ()
+    cpr::Response r =  cpr::Get(cpr::Url("http://httpbin.org/html"),cpr::Header({{"accept","text/html"}}));
 
+
+   int nStartHeader = r.text.find("<h1>" ,0);
+   int nFinishHeader = r.text.find("</h1>",0);
+
+    std::cout << r.text.substr(nStartHeader, (nFinishHeader - nStartHeader)+5) << std::endl;
 }
